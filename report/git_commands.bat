2d90b183968dd15e7eb535167c83294bf4b0f260
SaranJambu <tsaran412@gmail.com>
2024-06-24 17:26:15 +0530

    update report folder

--- report/git_commands.bat
+++ report/git_commands.bat
@@ -1,5 +1,5 @@
 cd /d D:\GIT\CI\Continuos-Integration\report 
 D: 
-git add report_4.xml 
-git commit -m "Adding the report_4.xml" 
+git add report_5.xml 
+git commit -m "Adding the report_5.xml" 
 git push 
